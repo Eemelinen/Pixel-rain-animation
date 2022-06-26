9d654b57dc465fbc80e637a66055baab798d4e17
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2022-06-26 12:58:43 +0200

    Animation working

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const myImage = new Image();
 myImage.src = stringifiedImage;
 
-
 let particlesArray = [];
 const numberOfParticles = 5000;
 
@@ -12,18 +11,52 @@ function drawImage() {
   canvas.width = 570;
   canvas.height = 697;
   ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+  const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  let mappedImage = [];
+  for (let y = 0; y < canvas.height; y++) {
+    let row = [];
+    for (let x = 0; x < canvas.width; x++) {
+      const red = pixels.data[(y * 4 * pixels.width) + (x * 4)];
+      const green = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)];
+      const blue = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)];
+      const brightness = calculateRelativeBrightness(red, green, blue);
+      const cell = [
+        brightness,
+      ];
+      row.push(cell);
+    }
+    mappedImage.push(row);
+  }
+
+  /** Adjust colors for human eye perception */
+  function calculateRelativeBrightness(red, green, blue) {
+    return Math.sqrt(
+      (red * red) * 0.299 +
+      (green * green) * 0.587 +
+      (blue * blue) * 0.114
+    ) / 100;
+  }
 
   class Particle {
     constructor() {
       this.x = Math.random() * canvas.width;
       this.y = 0;
       this.speed = 0;
-      this.velocity = Math.random() * 3.5;
+      this.velocity = Math.random() * 0.5;
       this.size = Math.random() * 1.5 + 1;
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
     }
 
     update() {
-      this.y += this.velocity;
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
+      this.speed = mappedImage[this.position1][this.position2][0];
+      let movement = (2.5 - this.speed) + this.velocity;
+
+      this.y += movement;
       if (this.y >= canvas.height) {
         this.y = 0;
         this.x = Math.random() * canvas.width;
@@ -47,13 +80,14 @@ function drawImage() {
   initParticles();
 
   function animate() {
-    ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
     ctx.globalAlpha = 0.05;
     ctx.fillStyle = 'rgb(0, 0, 0)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = 0.2;
 
     for (let i = 0; i < particlesArray.length; i++) {
       particlesArray[i].update();
+      ctx.globalAlpha = particlesArray[i].speed * 0.5;
       particlesArray[i].draw();
     }
     requestAnimationFrame(animate);
